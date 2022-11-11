9f7a219b2258c10eaeadc5881f23023b27412976
rubayet-islam <season.ss2001@gmail.com>
2022-11-11 21:59:24 +0600

    banner edited

--- src/Components/Pages/Banner.js
+++ src/Components/Pages/Banner.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import chair from '../../assets/images/chair.png';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200">
+        <div className="hero bg-chair">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" alt='' />
+          <img src={chair} className="rounded-lg w-1/2 shadow-2xl" alt='' />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
           </div>
         </div>
       </div>
