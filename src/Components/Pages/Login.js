061e74002d66cdfe0c82e78d49826e1ae368f479
rubayet-islam <season.ss2001@gmail.com>
2022-11-14 23:00:02 +0600

    react hook setup done with a basic login form setup

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+    const {register, handleSubmit} = useForm();
+    const [data, setData] = useState('');
+
+
     return (
-        <div>
-            this is login
+        <div className='h-[400px] flex justify-center items-center'>
+            <div>
+                <h2 className='text-4xl'>Login</h2>
+                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+
+      <div className="form-control w-full max-w-xs">
+  <label className="label"> <span className="label-text">Your Email</span></label>
+  <input  type="text" className="input input-bordered w-full max-w-xs" {...register("email")} />
+</div>
+      <div className="form-control w-full max-w-xs">
+  <label className="label"> <span className="label-text">Your Password</span></label>
+  <input  type="password" className="input input-bordered w-full max-w-xs" {...register("password")} />
+</div>
+      <p>{data}</p>
+      <input type="submit" />
+    </form>
+            </div>
         </div>
     );
 };
