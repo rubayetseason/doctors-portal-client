8b7989d0616d82babd31085a7786daae5e0f1b1e
rubayet-islam <season.ss2001@gmail.com>
2022-11-22 19:51:50 +0600

    manage doctor page created with doctors data display

--- src/Components/Dashboard/AddDoctor/ManageDoctors.js
+++ src/Components/Dashboard/AddDoctor/ManageDoctors.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 const ManageDoctors = () => {
-    return (
-        <div>
-            manage doctors here
-        </div>
-    );
+  const { data: doctors } = useQuery({
+    queryKey: ["doctors"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("http://localhost:5000/doctors", {
+          headers: {
+            authorization: `bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        const data = await res.json();
+        return data;
+      } catch (error) {}
+    },
+  });
+
+  return (
+    <div>
+      <h3 className="text-3xl mb-5">Manage Doctors: {doctors?.length}</h3>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Specialty</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+           {
+            doctors.map((doctor, i) =>  <tr key={doctor._id}>
+                <th>{i + 1}</th>
+                <td><div className="avatar">
+  <div className="w-24 rounded-full">
+    <img src={doctor.image} alt="" />
+  </div>
+</div></td>
+                <td>{doctor.name}</td>
+                <td>{doctor.email}</td>
+                <td>{doctor.specialty}</td>
+                <td><button className="btn btn-error btn-sm">Delete</button></td>
+              </tr>)
+           }
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageDoctors;
