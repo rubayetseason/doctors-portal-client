6e8bea2cb245bad9946e6fa4e3fc07736cca24a5
rubayet-islam <season.ss2001@gmail.com>
2022-11-22 22:10:41 +0600

    stripe method initialized

--- src/Components/Dashboard/Payment.js
+++ src/Components/Dashboard/Payment.js
@@ -3,10 +3,13 @@ import { useLoaderData } from 'react-router-dom';
 
 const Payment = () => {
     const booking = useLoaderData();
+    const {treatment, appointmentDate, slot, price} = booking;
     console.log(booking);
     return (
         <div>
-           <h3 className="text-2xl">This is payment</h3>
+           <h3 className="text-2xl font-semibold">Payment for {treatment}</h3>
+           <p className='mt-4'><strong>$ {price}</strong> is due for your appointment on {appointmentDate} at {slot}</p>
+
         </div>
     );
 };
--- src/Components/Shared/DisplayError.js
+++ src/Components/Shared/DisplayError.js
@@ -18,7 +18,7 @@ const DisplayError = () => {
     return (
         <div>
             <p className='text-red-500'>Something went wrong!!!</p>
-            <p className='text-red-400'>{error.statusText || error.message}</p>
+            <p className='text-red-500'>{error.statusText || error.message}</p>
             <h4 className="text-3xl"> Please <button onClick={handleLogOut}>Sign out</button> and log back in</h4>
         </div>
     );
