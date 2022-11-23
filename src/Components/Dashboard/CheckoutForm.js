ec39bdadc8861ea841f1df39f94308ac2408fa3b
rubayet-islam <season.ss2001@gmail.com>
2022-11-23 20:27:14 +0600

    stripe cardelement added

---
+++ src/Components/Dashboard/CheckoutForm.js
@@ -0,0 +1,65 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useState } from 'react';
+
+const CheckoutForm = () => {
+    const [cardError, setCardError] = useState('');
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if(!stripe || !elements){
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+        if(card === null){
+            return;
+        }
+
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+        if(error){
+console.log(error);
+setCardError(error.message);
+        }
+        else{
+            setCardError('')
+        }
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+        <CardElement
+            options={{
+                style: {
+                    base: {
+                        fontSize: '16px',
+                        color: '#424770',
+                        '::placeholder': {
+                            color: '#aab7c4',
+                        },
+                    },
+                    invalid: {
+                        color: '#9e2146',
+                    },
+                },
+            }}
+        />
+        <button
+            className='btn btn-sm btn-outline mt-4'
+            type="submit"
+            disabled={!stripe}>
+            Pay
+        </button>
+    </form>
+    <p className='text-red-600'>{cardError}</p>
+    </>
+    );
+};
+
+export default CheckoutForm;
--- src/Components/Dashboard/Payment.js
+++ src/Components/Dashboard/Payment.js
@@ -1,15 +1,25 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
     const booking = useLoaderData();
     const {treatment, appointmentDate, slot, price} = booking;
     console.log(booking);
     return (
-        <div>
+        <div className='w-3/4'>
            <h3 className="text-2xl font-semibold">Payment for {treatment}</h3>
            <p className='mt-4'><strong>$ {price}</strong> is due for your appointment on {appointmentDate} at {slot}</p>
-
+<div className='w-1/2 mt-5'>
+<Elements stripe={stripePromise}>
+                    <CheckoutForm
+                    />
+                </Elements>
+</div>
         </div>
     );
 };
